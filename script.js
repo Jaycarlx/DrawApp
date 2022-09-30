3d9e5c4ee2661048c52dcca68a454d94ef0119a9
Jaycarlx <molokwuchibuzorchristian@gmail.com>
2022-09-30 23:27:01 +0100

    DrawApp

--- script.js
+++ script.js
@@ -1,38 +1,108 @@
- const canvas = document.querySelector("canvas"),
- toolBtns = document.querySelectorAll(".tool")
+const canvas = document.querySelector("canvas"),
+    toolBtns = document.querySelectorAll(".tool"),
+    fillColor = document.querySelector("#fill-color"),
+    sizeSlider = document.querySelector("#size-slider"),
+    colorBtns = document.querySelectorAll(".colors .option"),
+    clearAll = document.querySelector(".clear-canvas")
+    colorPicker = document.querySelector("#color-picker"),
     ctx = canvas.getContext("2d");
 
 
-let isDrawing = false,
+let prevMouseX, prevMouseY, snapShot,
+    isDrawing = false,
     selectedTool = "brush"
-    brushWidth = 5;
+brushWidth = 5;
+selectedColor = "#000"
 
 window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight
 })
 
+const drawRect = (e) => {
+    if (!fillColor.checked) {
+       return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
+    } else {
+        ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY)
+    }
+}
+
+const drawCircle = (e) => {
+    ctx.beginPath();
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke()
+} 
+
+const drawTriangle = (e) => {
+    ctx.beginPath();
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+    ctx.closePath()
+    fillColor.checked ? ctx.fill() : ctx.stroke()
+} 
 
 const startDraw = (e) => {
-    e.preventDefault()
     isDrawing = true;
+    prevMouseX = e.offsetX;
+    prevMouseY = e.offsetY
     ctx.beginPath()
-    ctx.lineWidth = brushWidth
+    ctx.lineWidth = brushWidth;
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;
+    snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
 const drawing = (e) => {
     if (!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+    ctx.putImageData(snapShot, 0, 0)
+    if (selectedTool === "brush" || selectedTool === "eraser") {
+        ctx.strokeStyle = selectedTool === "brush" ? selectedColor : "#fff"
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    } else if (selectedTool === "rectangle") {
+        drawRect(e)
+    }
+    else if (selectedTool === "circle") {
+        drawCircle(e)
+    }
+    else if (selectedTool === "triangle") {
+        drawTriangle(e)
+    }
 }
 
 toolBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         document.querySelector(".options .active").classList.remove("active")
-        btn.classList.add("active")
+        btn.classList.add("active");
+        selectedTool = btn.id;
         console.log(btn.id)
     })
 })
+
+sizeSlider.addEventListener("change", () => {
+    brushWidth = sizeSlider.value
+    console.log(sizeSlider.value)
+})
+
+colorBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector(".options .selected").classList.remove("selected")
+        btn.classList.add("selected");
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+})
+})
+
+colorPicker.addEventListener("change", () => {
+    colorPicker.parentElement.style.backgroundColor = colorPicker.value;
+    colorPicker.parentElement.click()
+})
+
+clearAll.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
+
 
 canvas.addEventListener("mousedown", startDraw, false);
 canvas.addEventListener("mousemove", drawing, false);
@@ -48,8 +118,8 @@ console.log(canvas);
 
 //let canvas = document.querySelector("canvas");
 //     ctx = canvas.getContext("2d");
-ctx.strokeStyle = "#222222";
-ctx.lineWith = 5;
+// ctx.strokeStyle = "#222222";
+// ctx.lineWith = 5;
     
 // Set up mouse events for draWing
 var draWing = false;
